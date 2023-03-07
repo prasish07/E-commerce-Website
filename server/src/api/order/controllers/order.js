de59e4ad09e0b5fde5679d4ec63776db8809a374
prasish <prasishshrestha0099@gmail.com>
2023-03-07 18:40:54 +0545

    first commit

---
+++ server/src/api/order/controllers/order.js
@@ -0,0 +1,47 @@
+"use strict";
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+/**
+ * order controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::order.order", ({ stripe }) => ({
+  async create(ctx) {
+    const { products, userName, email } = ctx.request.body;
+
+    try {
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const item = await strapi
+            .service("api::item.item")
+            .findOne(product.id);
+
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name,
+              },
+              unit_amount: item.price * 100,
+            },
+            quantity: product.count,
+          };
+        })
+      );
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        customer_email: email,
+        mode: "payment",
+        success_url: "http://localhost:3000/checkout/success",
+        cancel_url: "http://localhost:3000",
+        line_items: lineItems,
+      });
+
+      await strapi.service("api::order.order").create({
+        data: { userName, products, strapiSessionId: session.id },
+      });
+    } catch (error) {}
+  },
+}));
